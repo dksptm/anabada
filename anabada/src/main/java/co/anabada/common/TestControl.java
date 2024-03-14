470ac662f4fe32dcda0af6da4b0582d3c33b5e4e
admin <admin@DESKTOP-NUNO0P1>
2024-03-14 15:58:23 +0900

    123

--- anabada/src/main/java/co/anabada/common/TestControl.java
+++ anabada/src/main/java/co/anabada/common/TestControl.java
@@ -2,6 +2,7 @@ package co.anabada.common;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,8 +11,9 @@ public class TestControl implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-
+		String path = "main/test.tiles";
+		RequestDispatcher dispatch = req.getRequestDispatcher(path);
+		dispatch.forward(req, resp);
 	}
 
 }
