c30c0d4d4cbd30bbccbfa876b37848bdecae9b1a
admin <admin@DESKTOP-NUNO0P1>
2024-03-14 14:35:05 +0900

    bada.

---
+++ anabada/src/main/java/co/anabada/common/TestControl.java
@@ -0,0 +1,17 @@
+package co.anabada.common;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestControl implements Control {
+
+	@Override
+	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+
+	}
+
+}
