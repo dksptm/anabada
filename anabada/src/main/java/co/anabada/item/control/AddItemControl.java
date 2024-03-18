566a628bd2a1dca86cdc9eb843f83a091c9393f1
admin <admin@DESKTOP-HEF96GB>
2024-03-18 17:50:09 +0900

    0318 마지막

--- anabada/src/main/java/co/anabada/item/control/AddItemControl.java
+++ anabada/src/main/java/co/anabada/item/control/AddItemControl.java
@@ -33,6 +33,7 @@ public class AddItemControl implements Control {
 		MultipartRequest multi = 
 				new MultipartRequest(req, savePath, maxSize, enc, new DefaultFileRenamePolicy());
 		
+		
 		// 이제 req가 아니라 multi에서 파라미터를 가져옴.
 		String cate = multi.getParameter("cate");
 		String memNum = multi.getParameter("mem_num");
